837954e859489d7c9e7c3b0c477d13c8ca649d04
VacheslavMerkotan <Nihil_13@mail.ru>
2022-05-09 21:00:14 +0300

    Initial commit

---
+++ src/Lesson4/Car.java
@@ -0,0 +1,18 @@
+package Lesson4;
+
+public class Car {
+    Car(String cvet, String motor) {
+        color = cvet;
+        engine = motor;
+    }
+    String color;
+    String engine;
+}
+class CarTest {
+    public static void main(String[] args) {
+        Car car1 = new Car("yellow", "V4");
+        System.out.println(car1.color);
+        System.out.println(car1.engine);
+
+    }
+}
