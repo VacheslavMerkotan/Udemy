b6e0e45a02d1214154675d2e06b41baad0abacf2
VacheslavMerkotan <Nihil_13@mail.ru>
2022-05-10 11:02:19 +0300

    Metod Popolneniy i Snyatie

--- src/ClassObekt/BankAccount.java
+++ src/ClassObekt/BankAccount.java
@@ -5,12 +5,21 @@ public class BankAccount {
     String name = "Peter";
     double ballance = 100;
 
+    double popolnenieScheta(double summaPopolnenia) {
+        ballance += summaPopolnenia;
+        return ballance;
+    }
+
+    double snyatieSoScheta(double summaSnyatia) {
+        ballance -= summaSnyatia;
+        return ballance;
+    }
+
     void showInfo() {
         System.out.println("Id: " + id + ", " + "Name: " + name + ", " + "Ballance: " + ballance);
     }
-
-
 }
+
 class BankAccountTest{
     public static void main(String[] args){
 
@@ -31,12 +40,26 @@ class BankAccountTest{
             HisAccount.ballance = 8.40;
 
             MyAccount.showInfo();
+            MyAccount.popolnenieScheta(350.89);
+            MyAccount.showInfo();
+            MyAccount.snyatieSoScheta(281.94);
+            MyAccount.showInfo();
+            System.out.println();
+
             YorAccount.showInfo();
+            YorAccount.snyatieSoScheta(8.45);
+            YorAccount.showInfo();
+            YorAccount.popolnenieScheta(494.35);
+            YorAccount.showInfo();
+            System.out.println();
+
+            HisAccount.showInfo();
+            HisAccount.popolnenieScheta(593.78);
+            HisAccount.showInfo();
+            HisAccount.snyatieSoScheta(479.28);
             HisAccount.showInfo();
 
-           //System.out.println(MyAccount.id);
-           //System.out.println(MyAccount.name);
-           //System.out.println(MyAccount.ballance);
+
 
 
 
